0b888936552fd2e996aa1923bda7ba6acd08b45e
FernandoSeg-GH <segre.fernando@gmail.com>
2024-10-06 11:32:10 +0200

    Signout callback

--- components/user/UserMenu.tsx
+++ components/user/UserMenu.tsx
@@ -52,7 +52,7 @@ export default function UserMenu() {
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                    <Button variant="ghost" className="flex w-full justify-start p-1 items-center hover:bg-red-50" onClick={() => signOut()}>
+                    <Button variant="ghost" className="flex w-full justify-start p-1 items-center hover:bg-red-50" onClick={() => signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_URL}/auth/sign-in` })}>
                         <MdClose className="mr-2" /> Logout
                     </Button>
                 </DropdownMenuItem>
